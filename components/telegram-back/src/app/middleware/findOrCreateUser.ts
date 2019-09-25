6ce087dca2a925a21ca456d3a1f9c1fcbda7bd8d
iskryzhytskyi <a.iskryzhytskyi@gmail.com>
2019-09-25 23:56:00 +0300

    fix username

--- components/telegram-back/src/app/middleware/findOrCreateUser.ts
+++ components/telegram-back/src/app/middleware/findOrCreateUser.ts
@@ -1,4 +1,5 @@
 import * as telegram from 'node-telegram-bot-api'
+import { isString } from 'lodash'
 import { Handler, HandlerResult, Message } from '../types'
 import { TelegramUserAttributes, TelegramUserModel, TelegramUserStatus } from 'libs/domain-model'
 
@@ -12,13 +13,14 @@ const handler: Handler = async (bot: telegram, msg: Message) => {
   const tUserId = from.id
   let user = await TelegramUserModel.findOne({ 'telegram.id': tUserId })
   if (!user) {
+    const username = isString(from.username) ? from.username : `${from.first_name} ${from.last_name}`.trim()
     const userData: TelegramUserAttributes = {
       status: TelegramUserStatus.unknown,
       telegram: {
+        username,
         id: tUserId,
         firstName: from.first_name,
         lastName: from.last_name,
-        username: from.username,
       },
     }
     user = await TelegramUserModel.create(userData)
