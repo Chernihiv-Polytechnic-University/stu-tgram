e53c4bb1d8ed6e734e7675517305a1c1a68aea44
iskryzhytskyi <a.iskryzhytskyi@gmail.com>
2020-02-25 19:15:45 +0200

    feat(overlord): update scripts

--- overlord/nodescripts/compile-libs.js
+++ overlord/nodescripts/compile-libs.js
@@ -1,4 +1,5 @@
-const { readdir, mkdir, lstat, copyFile } = require('fs').promises
+const { F_OK } = require('fs').constants
+const { readdir, mkdir, lstat, copyFile, access } = require('fs').promises
 const { promisify } = require('util')
 const { resolve, join } = require('path')
 const childProcess = require('child_process')
@@ -6,11 +7,19 @@ const version = require('./version')
 
 const exec = promisify(childProcess.exec)
 
+const FORCE_FLAG = '--f'
+
 if (!version.isTwelve()) {
     throw new Error('Node version should be >= 12.x')
 }
 
 const libsDir = resolve(__dirname, '..', '..', 'libs')
+
+const isDirExists = async (path) => {
+    return await access(path, F_OK)
+        .then(() => true)
+        .catch(() => false)
+}
 
 const mapSeries = (fn) => async (arr) => {
     for (let i = 0; i < arr.length; i++) {
@@ -18,11 +27,15 @@ const mapSeries = (fn) => async (arr) => {
     }
 }
 
-const findLibsToAdd = async () => {
+const findLibsAndFlags = async () => {
     const existedLibs = await readdir(libsDir)
     const requestedLibs = process.argv.slice(2)
 
-    return requestedLibs.filter(lib => existedLibs.includes(lib))
+    const force = requestedLibs.includes(FORCE_FLAG)
+
+    return requestedLibs
+        .filter(lib => existedLibs.includes(lib))
+        .map(libName => ({ libName, force }))
 }
 
 const copyFolder = async (from, to) => {
@@ -37,11 +50,19 @@ const copyFolder = async (from, to) => {
     }))
 }
 
-const addLib = async (libName, i, arr) => {
-    await exec(`npm run --prefix ${resolve(libsDir, libName)} compile`).catch(console.error)
+const addLib = async ({ libName, force }, i, arr) => {
+    const isDistExist = force ? false : await isDirExists(resolve(libsDir, libName, 'dist'))
+    const isModulesExist = force ? false : await isDirExists(resolve(libsDir, libName, 'node_modules'))
+
+    const command = (isDistExist && isModulesExist) ? 'compile-component' : 'compile'
+
+    console.log(`[ ${arr.length}:${i + 1} ]. ${ (isDistExist && isModulesExist) ? 'Only transpile and copy Lib' : 'Fully compile lib'}: ${libName}.`)
+
+    await exec(`npm run --prefix ${resolve(libsDir, libName)} ${command}`).catch(console.error)
     await copyFolder(resolve(libsDir, libName, 'dist'), resolve(process.cwd(), 'node_modules', 'libs', libName)).catch(console.error)
+
     console.log(`[ ${arr.length}:${i + 1} ]. Lib: ${libName} compiled.`)
 }
 
-findLibsToAdd().then(mapSeries(addLib))
+findLibsAndFlags().then(mapSeries(addLib))
 
--- overlord/nodescripts/cp.js
+++ overlord/nodescripts/cp.js
@@ -1,5 +1,5 @@
-const { copyFileSync, basename } = require('fs')
-const { resolve, join } = require('path')
+const { copyFileSync } = require('fs')
+const { resolve, basename, join } = require('path')
 const version = require('./version')
 
 if (!version.isTwelve()) {
@@ -9,7 +9,7 @@ if (!version.isTwelve()) {
 const target = resolve(process.cwd(), process.argv[2])
 
 process.argv.slice(3)
-    .map(path => ({ path: resolve(process.cwd(), path), name: basename(path) }))
+    .map(path => ({ path: join(process.cwd(), path), name: basename(path) }))
     .map(({ path, name }) => {
         copyFileSync(path, resolve(target, name))
     })
