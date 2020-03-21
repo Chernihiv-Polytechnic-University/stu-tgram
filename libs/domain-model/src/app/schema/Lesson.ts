f682453a6a423926f0b6e26f8dae30ee0f254a64
iskryzhytskyi <a.iskryzhytskyi@gmail.com>
2020-03-21 21:39:01 +0200

    feat(domain-model): teacher lessons feature

--- libs/domain-model/src/app/schema/Lesson.ts
+++ libs/domain-model/src/app/schema/Lesson.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose'
 
-export type LessonDay = 'ПН' | 'ВТ' | 'СР' | 'ЧТ' | 'ПТ' | 'СБ'
+export type LessonDay = 'ПН' | 'ВТ' | 'СР' | 'ЧТ' | 'ПТ' | 'СБ' | 'НД'
 export interface LessonAttributes {
   isExist: boolean,
   number: number,
@@ -11,6 +11,7 @@ export interface LessonAttributes {
   groupId: string,
   teacher?: {
     name?: string,
+    only?: boolean,
   },
 }
 
@@ -19,13 +20,17 @@ export interface Lesson extends LessonAttributes, mongoose.Document {}
 const schema = new mongoose.Schema({
   isExist: { type: Boolean, default: false },
   number: { type: Number, default: -1 },
-  day: { type: String, enum: ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'] },
+  day: { type: String, enum: ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'НД'] },
   name: { type: String },
   auditory: { type: String },
   week: { type: Number, default: 0, enum: [0, 1] },
   groupId: { type: String, index: true },
   teacher: {
     name: { type: String },
+    // it's simpler to store additional 3-4MB of data then trying to match
+    // lessons by teacher & group. Not sure my data sources are correct enough
+    // and have consistent data, so lessons are separated with this flag
+    only: { type: Boolean },
   },
 }, {
   collection: 'Lesson',
